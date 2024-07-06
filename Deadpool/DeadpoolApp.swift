597aaa7facf08e958b015d9ec4e58ce42e4c6a08
Burhan ARAS <burhanaras@live.com>
2024-07-06 12:44:29 -0700

    feat: Create initial version of the project
    Created a new tvOS project for Marvel heroes

---
+++ Deadpool/DeadpoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeadpoolApp.swift
+//  Deadpool
+//
+//  Created by Burhan Aras on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DeadpoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
